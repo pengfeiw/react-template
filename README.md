465c975657ee6368b51863f6344f84143142834a
fangyan <fangyan@qunhemail.com>
2023-03-18 14:31:37 +0800

    init project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Template for React
+
+## env
+
+```bash
+$ node -v
+v14.7.0
+```
+
+## feature
+
+- webpack
+- react
+- typescript
+- less
+- babel
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.less";
+
+const App = () => {
+    return (
+        <div className="title">React Template</div>
+    );
+};
+
+export default App;
